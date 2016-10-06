49c03cbcf8d26426a4ddb37416a8ecd9e4918913
Pål Edman <pal.edman@gmail.com>
2016-10-06 07:13:41 +0200

    Added tests for script-helper

--- test/tasks/task-loop-test.js
+++ test/tasks/task-loop-test.js
@@ -127,7 +127,7 @@ lab.experiment('task loop', () => {
       });
     });
 
-    lab.test('loops user task until condition is reached', (done) => {
+    lab.test('loops user task until condition is met', (done) => {
       const def = `
   <bpmn:definitions id= "Definitions_1" xmlns:bpmn="http://www.omg.org/spec/BPMN/20100524/MODEL"
       xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" targetNamespace="http://bpmn.io/schema/bpmn">
@@ -196,50 +196,51 @@ lab.experiment('task loop', () => {
         });
       });
     });
+
+    lab.test('on recurring task error the loop breaks', (done) => {
+      const def = `
+  <bpmn:definitions id= "definitions" xmlns:bpmn="http://www.omg.org/spec/BPMN/20100524/MODEL"
+    xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" targetNamespace="http://bpmn.io/schema/bpmn">
+    <bpmn:process id="taskLoopProcess" isExecutable="true">
+      <bpmn:scriptTask id="recurring" name="Recurring" scriptFormat="JavaScript">
+        <bpmn:multiInstanceLoopCharacteristics isSequential="true">
+          <bpmn:loopCardinality xsi:type="bpmn:tFormalExpression">7</bpmn:loopCardinality>
+        </bpmn:multiInstanceLoopCharacteristics>
+        <bpmn:script><![CDATA[
+          context.input += context.items[index];
+          if (index >= 2) next(new Error('Three is enough'));
+          else next();
+          ]]>
+        </bpmn:script>
+      </bpmn:scriptTask>
+      <bpmn:boundaryEvent id="errorEvent" attachedToRef="recurring">
+        <bpmn:errorEventDefinition />
+      </bpmn:boundaryEvent>
+    </bpmn:process>
+  </bpmn:definitions>
+    `;
+
+      const engine = new Bpmn.Engine(def);
+      const listener = new EventEmitter();
+
+      let startCount = 0;
+      listener.on('start-recurring', () => {
+        startCount++;
+      });
+
+      engine.startInstance({
+        input: 0,
+        items: [0].concat(Array(10).fill(7))
+      }, listener, (err, instance) => {
+        if (err) return done(err);
+
+        instance.once('end', () => {
+          expect(startCount, 'number of start').to.equal(3);
+          expect(instance.variables.input).to.equal(14);
+          testHelper.expectNoLingeringListeners(instance);
+          done();
+        });
+      });
+    });
   });
-
-//   lab.describe('parallel', () => {
-//     const sequential = `
-// <bpmn:definitions id= "Definitions_1" xmlns:bpmn="http://www.omg.org/spec/BPMN/20100524/MODEL"
-//   xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" targetNamespace="http://bpmn.io/schema/bpmn">
-//   <bpmn:process id="taskLoopProcess" isExecutable="true">
-//     <bpmn:startEvent id="start" />
-//     <bpmn:sequenceFlow id="flow1" sourceRef="start" targetRef="recurring" />
-//     <bpmn:scriptTask id="recurring" name="Recurring" scriptFormat="JavaScript">
-//       <bpmn:multiInstanceLoopCharacteristics />
-//       <bpmn:script><![CDATA[
-//         setTimeout(next, Math.floor(Math.random() * 7))
-//         ]]>
-//       </bpmn:script>
-//     </bpmn:scriptTask>
-//     <bpmn:endEvent id="end" />
-//     <bpmn:sequenceFlow id="flow2" sourceRef="recurring" targetRef="end" />
-//   </bpmn:process>
-// </bpmn:definitions>
-//   `;
-
-//     lab.test('runs a looped script task until it has finished', (done) => {
-//       const engine = new Bpmn.Engine(sequential);
-//       const listener = new EventEmitter();
-
-//       let startCount = 0;
-//       listener.on('start-recurring', () => {
-//         startCount++;
-//       });
-
-//       engine.startInstance({
-//         items: Array(10).fill(5),
-//         setTimeout: setTimeout
-//       }, listener, (err, instance) => {
-//         if (err) return done(err);
-
-//         instance.once('end', () => {
-//           expect(startCount).to.equal(10);
-//           testHelper.expectNoLingeringListeners(instance);
-//           done();
-//         });
-//       });
-//     });
-
-//   });
 });
