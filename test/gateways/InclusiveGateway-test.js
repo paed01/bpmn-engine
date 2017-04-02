c274854074b6ac9fbd2240d448b25a8552a1fe72
Pål Edman <pal.edman@gmail.com>
2017-04-02 07:50:26 +0200

    Add test to make sure inclusive gateway respect input

--- test/gateways/InclusiveGateway-test.js
+++ test/gateways/InclusiveGateway-test.js
@@ -2,14 +2,66 @@
 
 const Code = require('code');
 const Lab = require('lab');
+const testHelpers = require('../helpers/testHelpers');
 
 const lab = exports.lab = Lab.script();
 const expect = Code.expect;
 const Bpmn = require('../..');
 
 lab.experiment('InclusiveGateway', () => {
-  lab.test('should have inbound and outbound sequence flows', (done) => {
+  lab.describe('behavior', () => {
     const processXml = `
+  <?xml version="1.0" encoding="UTF-8"?>
+  <definitions id="Definitions_1" xmlns="http://www.omg.org/spec/BPMN/20100524/MODEL"
+     xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:camunda="http://camunda.org/schema/1.0/bpmn" targetNamespace="http://bpmn.io/schema/bpmn">
+    <process id="mainProcess" isExecutable="true">
+      <inclusiveGateway id="decision" default="defaultFlow">
+        <extensionElements>
+          <camunda:InputOutput>
+            <camunda:inputParameter name="takeCondition">\${variables.condition1}</camunda:inputParameter>
+          </camunda:InputOutput>
+        </extensionElements>
+      </inclusiveGateway>
+      <endEvent id="end1" />
+      <endEvent id="end2" />
+      <sequenceFlow id="condFlow" sourceRef="decision" targetRef="end1">
+        <conditionExpression xsi:type="tFormalExpression">\${takeCondition}</conditionExpression>
+      </sequenceFlow>
+      <sequenceFlow id="defaultFlow" sourceRef="decision" targetRef="end2" />
+    </process>
+  </definitions>
+        `;
+
+    let context;
+    lab.beforeEach((done) => {
+      testHelpers.getContext(processXml, {
+        camunda: require('camunda-bpmn-moddle/resources/camunda')
+      }, (err, c) => {
+        if (err) return done(err);
+        context = c;
+        done();
+      });
+    });
+
+    lab.describe('input', () => {
+      lab.test('is passed to conditional flow', (done) => {
+        const gateway = context.getChildActivityById('decision');
+        gateway.activate();
+
+        context.variables.condition1 = true;
+
+        gateway.outbound.find((f) => f.id === 'condFlow').once('taken', () => {
+          done();
+        });
+
+        gateway.run();
+      });
+    });
+  });
+
+  lab.describe('engine behavior', () => {
+    lab.test('should have inbound and outbound sequence flows', (done) => {
+      const processXml = `
 <?xml version="1.0" encoding="UTF-8"?>
   <definitions xmlns="http://www.omg.org/spec/BPMN/20100524/MODEL" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
   <process id="theProcess" isExecutable="true">
@@ -33,23 +85,23 @@ lab.experiment('InclusiveGateway', () => {
   </process>
 </definitions>`;
 
-    const engine = new Bpmn.Engine({
-      source: processXml
-    });
-    engine.getDefinition((err, definition) => {
-      if (err) return done(err);
+      const engine = new Bpmn.Engine({
+        source: processXml
+      });
+      engine.getDefinition((err, definition) => {
+        if (err) return done(err);
 
-      const gateway = definition.getChildActivityById('decision');
-      expect(gateway).to.include('inbound');
-      expect(gateway.inbound).to.have.length(1);
-      expect(gateway).to.include('outbound');
-      expect(gateway.outbound).to.have.length(3);
-      done();
+        const gateway = definition.getChildActivityById('decision');
+        expect(gateway).to.include('inbound');
+        expect(gateway.inbound).to.have.length(1);
+        expect(gateway).to.include('outbound');
+        expect(gateway.outbound).to.have.length(3);
+        done();
+      });
     });
-  });
 
-  lab.test('should support multiple conditional flows, case 1', (done) => {
-    const processXml = `
+    lab.test('should support multiple conditional flows, case 1', (done) => {
+      const processXml = `
 <?xml version="1.0" encoding="UTF-8"?>
   <definitions xmlns="http://www.omg.org/spec/BPMN/20100524/MODEL" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
   <process id="theProcess" isExecutable="true">
@@ -73,27 +125,27 @@ lab.experiment('InclusiveGateway', () => {
   </process>
 </definitions>`;
 
-    const engine = new Bpmn.Engine({
-      source: processXml
-    });
-    engine.execute({
-      variables: {
-        input: 1
-      }
-    }, (err, execution) => {
-      if (err) return done(err);
+      const engine = new Bpmn.Engine({
+        source: processXml
+      });
+      engine.execute({
+        variables: {
+          input: 1
+        }
+      }, (err, execution) => {
+        if (err) return done(err);
 
-      execution.on('end', () => {
-        expect(execution.getChildActivityById('theEnd1').taken, 'theEnd1').to.be.true();
-        expect(execution.getChildActivityById('theEnd2').taken, 'theEnd2').to.be.true();
-        expect(execution.getChildActivityById('theEnd3').taken, 'theEnd3').to.be.true();
-        done();
+        execution.on('end', () => {
+          expect(execution.getChildActivityById('theEnd1').taken, 'theEnd1').to.be.true();
+          expect(execution.getChildActivityById('theEnd2').taken, 'theEnd2').to.be.true();
+          expect(execution.getChildActivityById('theEnd3').taken, 'theEnd3').to.be.true();
+          done();
+        });
       });
     });
-  });
 
-  lab.test('should support the default flow in combination with multiple conditional flows, case condition met', (done) => {
-    const processXml = `
+    lab.test('should support the default flow in combination with multiple conditional flows, case condition met', (done) => {
+      const processXml = `
 <?xml version="1.0" encoding="UTF-8"?>
   <definitions xmlns="http://www.omg.org/spec/BPMN/20100524/MODEL" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
   <process id="theProcess" isExecutable="true">
@@ -117,27 +169,27 @@ lab.experiment('InclusiveGateway', () => {
   </process>
 </definitions>`;
 
-    const engine = new Bpmn.Engine({
-      source: processXml
-    });
-    engine.execute({
-      variables: {
-        input: 50
-      }
-    }, (err, execution) => {
-      if (err) return done(err);
+      const engine = new Bpmn.Engine({
+        source: processXml
+      });
+      engine.execute({
+        variables: {
+          input: 50
+        }
+      }, (err, execution) => {
+        if (err) return done(err);
 
-      execution.on('end', () => {
-        expect(execution.getChildActivityById('theEnd1').taken, 'theEnd1').to.be.false();
-        expect(execution.getChildActivityById('theEnd2').taken, 'theEnd2').to.be.true();
-        expect(execution.getChildActivityById('theEnd3').taken, 'theEnd3').to.be.false();
-        done();
+        execution.on('end', () => {
+          expect(execution.getChildActivityById('theEnd1').taken, 'theEnd1').to.be.false();
+          expect(execution.getChildActivityById('theEnd2').taken, 'theEnd2').to.be.true();
+          expect(execution.getChildActivityById('theEnd3').taken, 'theEnd3').to.be.false();
+          done();
+        });
       });
     });
-  });
 
-  lab.test('should support the default flow in combination with multiple conditional flows, case no conditions met', (done) => {
-    const processXml = `
+    lab.test('should support the default flow in combination with multiple conditional flows, case no conditions met', (done) => {
+      const processXml = `
 <?xml version="1.0" encoding="UTF-8"?>
   <definitions xmlns="http://www.omg.org/spec/BPMN/20100524/MODEL" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
   <process id="theProcess" isExecutable="true">
@@ -161,27 +213,27 @@ lab.experiment('InclusiveGateway', () => {
   </process>
 </definitions>`;
 
-    const engine = new Bpmn.Engine({
-      source: processXml
-    });
-    engine.execute({
-      variables: {
-        input: 60
-      }
-    }, (err, execution) => {
-      if (err) return done(err);
+      const engine = new Bpmn.Engine({
+        source: processXml
+      });
+      engine.execute({
+        variables: {
+          input: 60
+        }
+      }, (err, execution) => {
+        if (err) return done(err);
 
-      execution.on('end', () => {
-        expect(execution.getChildActivityById('theEnd1').taken, 'theEnd1').to.be.true();
-        expect(execution.getChildActivityById('theEnd2').taken, 'theEnd2').to.be.false();
-        expect(execution.getChildActivityById('theEnd3').taken, 'theEnd3').to.be.false();
-        done();
+        execution.on('end', () => {
+          expect(execution.getChildActivityById('theEnd1').taken, 'theEnd1').to.be.true();
+          expect(execution.getChildActivityById('theEnd2').taken, 'theEnd2').to.be.false();
+          expect(execution.getChildActivityById('theEnd3').taken, 'theEnd3').to.be.false();
+          done();
+        });
       });
     });
-  });
 
-  lab.test('emits error when no conditional flow is taken', (done) => {
-    const definitionXml = `
+    lab.test('emits error when no conditional flow is taken', (done) => {
+      const definitionXml = `
 <?xml version="1.0" encoding="UTF-8"?>
   <definitions xmlns="http://www.omg.org/spec/BPMN/20100524/MODEL" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
   <process id="theProcess" isExecutable="true">
@@ -203,21 +255,24 @@ lab.experiment('InclusiveGateway', () => {
   </process>
 </definitions>`;
 
-    const engine = new Bpmn.Engine({
-      source: definitionXml
-    });
-    engine.once('error', (err, gateway) => {
-      expect(err).to.be.an.error(/no conditional flow/i);
-      expect(gateway).to.include({id: 'decision'});
-      done();
-    });
+      const engine = new Bpmn.Engine({
+        source: definitionXml
+      });
+      engine.once('error', (err, gateway) => {
+        expect(err).to.be.an.error(/no conditional flow/i);
+        expect(gateway).to.include({
+          id: 'decision'
+        });
+        done();
+      });
 
-    engine.execute({
-      variables: {
-        input: 61
-      }
-    }, (err) => {
-      if (err) return done(err);
+      engine.execute({
+        variables: {
+          input: 61
+        }
+      }, (err) => {
+        if (err) return done(err);
+      });
     });
   });
 });
