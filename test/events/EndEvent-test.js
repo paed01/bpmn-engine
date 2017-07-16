19463dfda1fcde5fa038cfeb3d360dd70eb18a43
Pål Edman <pal.edman@gmail.com>
2017-07-16 08:58:18 +0200

    Add event sequence tests for EndEvent

--- test/events/EndEvent-test.js
+++ test/events/EndEvent-test.js
@@ -93,71 +93,234 @@ describe('EndEvent', () => {
       event.activate();
       event.inbound[0].take();
     });
+
+    describe('getState()', () => {
+      it('returns expected state on enter', (done) => {
+        const event = context.getChildActivityById('end');
+        event.once('enter', (activityApi, executionContext) => {
+          const state = activityApi.getApi(executionContext).getState();
+          expect(state).to.equal({
+            id: 'end',
+            type: 'bpmn:EndEvent',
+            entered: true
+          });
+          done();
+        });
+
+        event.activate();
+        event.inbound[0].take();
+      });
+
+      it('returns expected state on start', (done) => {
+        const event = context.getChildActivityById('end');
+        event.once('start', (activityApi, executionContext) => {
+          const state = activityApi.getApi(executionContext).getState();
+          expect(state).to.equal({
+            id: 'end',
+            type: 'bpmn:EndEvent',
+            entered: true
+          });
+          done();
+        });
+
+        event.activate();
+        event.inbound[0].take();
+      });
+
+      it('returns expected state on end', (done) => {
+        const event = context.getChildActivityById('end');
+        event.once('end', (activityApi, executionContext) => {
+          const state = activityApi.getApi(executionContext).getState();
+          expect(state).to.equal({
+            id: 'end',
+            type: 'bpmn:EndEvent',
+            taken: true
+          });
+          done();
+        });
+
+        event.activate();
+        event.inbound[0].take();
+      });
+
+      it('returns expected state on leave', (done) => {
+        const event = context.getChildActivityById('end');
+        event.once('leave', (activityApi, executionContext) => {
+          const state = activityApi.getApi(executionContext).getState();
+          expect(state).to.equal({
+            id: 'end',
+            type: 'bpmn:EndEvent',
+            taken: true
+          });
+          done();
+        });
+
+        event.activate();
+        event.inbound[0].take();
+      });
+    });
   });
 
-  describe('engine', () => {
-    describe('terminateEventDefinition', () => {
-      const source = `
-      <?xml version="1.0" encoding="UTF-8"?>
-        <definitions xmlns="http://www.omg.org/spec/BPMN/20100524/MODEL" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
-        <process id="theProcess" isExecutable="true">
-          <startEvent id="theStart" />
-          <endEvent id="fatal">
-            <terminateEventDefinition />
-          </endEvent>
-          <endEvent id="theEnd1" />
-          <endEvent id="theEnd2" />
-          <endEvent id="theEnd3" />
-          <sequenceFlow id="flow1" sourceRef="theStart" targetRef="fatal" />
-          <sequenceFlow id="flow2" sourceRef="theStart" targetRef="theEnd1" />
-          <sequenceFlow id="flow3" sourceRef="theStart" targetRef="theEnd2" />
-          <sequenceFlow id="flow4" sourceRef="theStart" targetRef="theEnd3" />
-        </process>
-      </definitions>`;
+  describe('TerminateEventDefinition', () => {
+    const source = `
+    <?xml version="1.0" encoding="UTF-8"?>
+      <definitions xmlns="http://www.omg.org/spec/BPMN/20100524/MODEL" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
+      <process id="theProcess" isExecutable="true">
+        <startEvent id="theStart" />
+        <endEvent id="fatal">
+          <terminateEventDefinition />
+        </endEvent>
+        <endEvent id="theEnd1" />
+        <endEvent id="theEnd2" />
+        <endEvent id="theEnd3" />
+        <sequenceFlow id="flow1" sourceRef="theStart" targetRef="fatal" />
+        <sequenceFlow id="flow2" sourceRef="theStart" targetRef="theEnd1" />
+        <sequenceFlow id="flow3" sourceRef="theStart" targetRef="theEnd2" />
+        <sequenceFlow id="flow4" sourceRef="theStart" targetRef="theEnd3" />
+      </process>
+    </definitions>`;
 
-      let definition;
-      lab.before((done) => {
-        const engine = new Engine({
-          source
-        });
-        engine.getDefinition((err, def) => {
-          if (err) return done(err);
-          definition = def;
-          done();
-        });
+    let context;
+    beforeEach((done) => {
+      testHelpers.getContext(source, {
+        camunda: require('camunda-bpmn-moddle/resources/camunda')
+      }, (err, c) => {
+        if (err) return done(err);
+        context = c;
+        done();
+      });
+    });
+
+    it('should terminate process', (done) => {
+      const engine = new Engine({
+        source
+      });
+      const listener = new EventEmitter();
+      listener.once('end-theEnd1', (activityApi) => {
+        fail(new Error(`${activityApi.id} should have been terminated`));
       });
 
-      it('should have inbound sequence flows', (done) => {
-        const element = definition.getChildActivityById('fatal');
-        expect(element).to.include('inbound');
-        expect(element.inbound).to.have.length(1);
+      engine.execute({
+        listener: listener
+      });
+
+      engine.once('end', (def) => {
+        expect(def.isEnded).to.equal(true);
+        testHelpers.expectNoLingeringListenersOnEngine(engine);
+        done();
+      });
+    });
+
+    it('should have inbound sequence flows', (done) => {
+      const element = context.getChildActivityById('fatal');
+      expect(element).to.include('inbound');
+      expect(element.inbound).to.have.length(1);
+      done();
+    });
+
+    it('and have terminate flag', (done) => {
+      const element = context.getChildActivityById('fatal');
+      expect(element.terminate).to.be.true();
+      done();
+    });
+
+    it('emits events in expected sequence', (done) => {
+      const event = context.getChildActivityById('fatal');
+      const sequence = [];
+
+      event.on('enter', (a, b) => {
+        expect(a.id).to.equal(b.id);
+        sequence.push('enter');
+      });
+      event.on('start', (a, b) => {
+        expect(a.id).to.equal(b.id);
+        sequence.push('start');
+      });
+      event.on('end', (a, b) => {
+        expect(a.id).to.equal(b.id);
+        sequence.push('end');
+      });
+      event.on('leave', (a, b) => {
+        expect(a.id).to.equal(b.id);
+        sequence.push('leave');
+      });
+      event.on('terminate', (a, b) => {
+        expect(a.id).to.equal(b.id);
+        sequence.push('terminate');
+        expect(sequence).to.equal(['enter', 'start', 'end', 'terminate']);
         done();
       });
 
-      it('and have property isTermation flag true', (done) => {
-        const element = definition.getChildActivityById('fatal');
-        expect(element.terminate).to.be.true();
-        done();
-      });
+      event.activate();
+      event.inbound[0].take();
+    });
 
-      it('should terminate process', (done) => {
-        const engine = new Engine({
-          source
-        });
-        const listener = new EventEmitter();
-        listener.once('end-theEnd1', (activityApi) => {
-          fail(new Error(`${activityApi.id} should have been terminated`));
-        });
-
-        engine.execute({
-          listener: listener
-        });
-
-        engine.once('end', (def) => {
-          expect(def.isEnded).to.equal(true);
-          testHelpers.expectNoLingeringListenersOnEngine(engine);
+    describe('getState()', () => {
+      it('returns expected state on enter', (done) => {
+        const event = context.getChildActivityById('fatal');
+        event.once('enter', (activityApi, executionContext) => {
+          const state = activityApi.getApi(executionContext).getState();
+          expect(state).to.equal({
+            id: 'fatal',
+            type: 'bpmn:EndEvent',
+            entered: true
+          });
           done();
         });
+
+        event.activate();
+        event.inbound[0].take();
+      });
+
+      it('returns expected state on start', (done) => {
+        const event = context.getChildActivityById('fatal');
+        event.once('start', (activityApi, executionContext) => {
+          const state = activityApi.getApi(executionContext).getState();
+          expect(state).to.equal({
+            id: 'fatal',
+            type: 'bpmn:EndEvent',
+            terminate: true,
+            entered: true
+          });
+          done();
+        });
+
+        event.activate();
+        event.inbound[0].take();
+      });
+
+      it('returns expected state on end', (done) => {
+        const event = context.getChildActivityById('fatal');
+        event.once('end', (activityApi, executionContext) => {
+          const state = activityApi.getApi(executionContext).getState();
+          expect(state).to.equal({
+            id: 'fatal',
+            type: 'bpmn:EndEvent',
+            terminate: true,
+            taken: true
+          });
+          done();
+        });
+
+        event.activate();
+        event.inbound[0].take();
+      });
+
+      it('returns expected state on terminate', (done) => {
+        const event = context.getChildActivityById('fatal');
+        event.once('terminate', (activityApi, executionContext) => {
+          const state = activityApi.getApi(executionContext).getState();
+          expect(state).to.equal({
+            id: 'fatal',
+            type: 'bpmn:EndEvent',
+            terminate: true,
+            taken: true
+          });
+          done();
+        });
+
+        event.activate();
+        event.inbound[0].take();
       });
     });
   });
