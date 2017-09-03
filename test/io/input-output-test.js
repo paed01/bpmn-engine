fb2ab70ca1c8a237725f6a9e027c5ae57c3429f2
Pål Edman <pal.edman@gmail.com>
2017-09-03 11:02:36 +0200

    Close io coverage issues

--- test/io/input-output-test.js
+++ test/io/input-output-test.js
@@ -23,6 +23,142 @@ describe('input/output', () => {
     });
   });
 
+  describe('activity io', () => {
+    it('getInput() with undefined io returns undefined', (done) => {
+      const task = context.getChildActivityById('StartEvent_1');
+      expect(task).to.include(['io']);
+      expect(task.io).to.exist();
+      expect(task.io.isDefault).to.equal(true);
+
+      expect(task.io.getInput()).to.be.undefined();
+
+      done();
+    });
+
+    it('getOutput() with undefined io returns undefined', (done) => {
+      const task = context.getChildActivityById('StartEvent_1');
+
+      expect(task.io.getOutput()).to.be.undefined();
+
+      done();
+    });
+
+    it('setOutputValue() assigns result', (done) => {
+      const task = context.getChildActivityById('StartEvent_1');
+
+      task.io.setOutputValue('name', 'me');
+      expect(task.io.getOutput()).to.equal({name: 'me'});
+
+      done();
+    });
+
+    it('setOutputValue() assigns result', (done) => {
+      const task = context.getChildActivityById('StartEvent_1');
+
+      task.io.setOutputValue('name', 'me');
+      expect(task.io.getOutput()).to.equal({name: 'me'});
+
+      done();
+    });
+
+    it('setOutputValue() assigns result property', (done) => {
+      const task = context.getChildActivityById('StartEvent_1');
+
+      task.io.setResult({
+        input: 1
+      });
+      task.io.setOutputValue('name', 'me');
+      expect(task.io.getOutput()).to.equal({
+        input: 1,
+        name: 'me'
+      });
+
+      done();
+    });
+  });
+
+  describe('element property io', () => {
+    it('getInput() returns empty', (done) => {
+      const task = context.getChildActivityById('errorBoundaryEvent');
+      expect(task).to.include(['io']);
+      expect(task.io).to.exist();
+
+
+      task.on('enter', (activityApi, activityExecution) => {
+        const errorDefIO = activityApi.getEvents()[0].io;
+        errorDefIO.setResult({
+          name: 'me'
+        });
+
+        const api = activityApi.getApi(activityExecution);
+        expect(api.getInput()).to.be.empty();
+        done();
+      });
+
+      task.activate().run();
+    });
+
+    it('getOutput() returns assigned result', (done) => {
+      const event = context.getChildActivityById('errorBoundaryEvent');
+
+      event.on('enter', (activityApi) => {
+        const errorDefIO = activityApi.getEvents()[0].io;
+        errorDefIO.setResult({
+          name: 'me'
+        });
+      });
+
+      event.on('end', (activityApi, activityExecution) => {
+        const api = activityApi.getApi(activityExecution);
+        expect(api.getOutput()).to.equal({
+          name: 'me',
+          errorMessage: 'Expected',
+          errorCode: undefined
+        });
+
+        done();
+      });
+
+      const eventActivity = event.activate();
+      eventActivity.attachedTo.activate().run({
+        error: 'Expected'
+      });
+    });
+  });
+
+  describe('camunda input/output', () => {
+    it('setOutputValue() assigns result', (done) => {
+      const task = context.getChildActivityById('serviceTask');
+
+      task.io.setOutputValue('result', [{
+        statusCode: 200
+      }, 'HTML']);
+      expect(task.io.getOutput()).to.equal({
+        statusCode: 200,
+        body: 'HTML'
+      });
+
+      done();
+    });
+
+    it('setOutputValue() assigns result property', (done) => {
+      const task = context.getChildActivityById('serviceTask');
+
+      task.io.setResult({
+        input: 1
+      });
+      task.io.setOutputValue('result', [{
+        statusCode: 200
+      }, 'HTML']);
+      expect(task.io.getOutput()).to.equal({
+        statusCode: 200,
+        body: 'HTML'
+      });
+
+      done();
+    });
+  });
+
   describe('service task with camunda input/output', () => {
     describe('getInput()', () => {
       it('return object with named input arguments', (done) => {
@@ -158,7 +294,8 @@ describe('input/output', () => {
         });
         expect(args).to.equal({
           input1: 2,
-          input2: '3'
+          input2: '3',
+          error: undefined
         });
 
         done();
@@ -242,6 +379,7 @@ describe('input/output', () => {
         expect(activityApi.getInput()).to.equal({
           input1: undefined,
           input2: '3',
+          error: undefined
         });
       });
 
