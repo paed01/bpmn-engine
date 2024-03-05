eafa9003ee443ea4abf7d26a188ae35dc01df96e
Pål Edman <pal.edman@gmail.com>
2024-03-05 11:33:34 +0100

    prototype ProcessOutputDataObject
    and make properties id and type readonly

--- src/extensions/ProcessOutputDataObject.js
+++ src/extensions/ProcessOutputDataObject.js
@@ -1,25 +1,41 @@
+const kDataObjectDef = Symbol.for('data object definition');
+
 export default function ProcessOutputDataObject(dataObjectDef, { environment }) {
-  const { id, type, name, behaviour, parent } = dataObjectDef;
-
-  const source = {
-    id,
-    name,
-    type,
-    behaviour,
-    parent,
-    read(broker, exchange, routingKeyPrefix, messageProperties) {
-      const value = environment.variables.data && environment.variables.data[id];
-      return broker.publish(exchange, `${routingKeyPrefix}response`, { id, name, type, value }, messageProperties);
-    },
-    write(broker, exchange, routingKeyPrefix, value, messageProperties) {
-      environment.variables.data = environment.variables.data || {};
-      environment.variables.data[id] = value;
-
-      environment.output.data = environment.output.data || {};
-      environment.output.data[id] = value;
-      return broker.publish(exchange, `${routingKeyPrefix}response`, { id, name, type, value }, messageProperties);
-    },
-  };
-
-  return source;
+  this[kDataObjectDef] = dataObjectDef;
+  this.environment = environment;
+  this.behaviour = dataObjectDef.behaviour;
+  this.name = dataObjectDef.name;
+  this.parent = dataObjectDef.parent;
 }
+
+Object.defineProperties(ProcessOutputDataObject.prototype, {
+  id: {
+    get() {
+      return this[kDataObjectDef].id;
+    },
+  },
+  type: {
+    get() {
+      return this[kDataObjectDef].type;
+    },
+  },
+});
+
+ProcessOutputDataObject.prototype.read = function readDataObject(broker, exchange, routingKeyPrefix, messageProperties) {
+  const environment = this.environment;
+  const { id, name, type } = this;
+  const value = environment.variables.data && environment.variables.data[this.id];
+  return broker.publish(exchange, `${routingKeyPrefix}response`, { id, name, type, value }, messageProperties);
+};
+
+ProcessOutputDataObject.prototype.write = function writeDataObject(broker, exchange, routingKeyPrefix, value, messageProperties) {
+  const environment = this.environment;
+  const { id, name, type } = this;
+
+  environment.variables.data = environment.variables.data || {};
+  environment.variables.data[id] = value;
+
+  environment.output.data = environment.output.data || {};
+  environment.output.data[id] = value;
+  return broker.publish(exchange, `${routingKeyPrefix}response`, { id, name, type, value }, messageProperties);
+};
